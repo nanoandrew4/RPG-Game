3573dff0ba12c10f2c8bc01d112bffadfe10306c
AianC <aechen0@gmail.com>
2016-05-30 22:16:53 -0600

    Tile for Main for InMap testing.

---
+++ InMap/Tile.java
@@ -0,0 +1,24 @@
+/*
+    Tile class for Rising Legend.
+ */
+
+package rlinmaptesting;
+
+public class Tile {
+    boolean isWall, stairUp, stairDown, openable;
+    
+    Tile(String entity) {
+        isWall = false;
+        stairUp = false;
+        stairDown = false;
+        openable = false;
+        
+        switch(entity) {
+            case "wall": isWall = true; break;
+            case "stairsUp": stairUp = true; break;
+            case "stairsDown": stairDown = true; break;
+            case "door": openable = true; isWall = true; break;
+            default: break;
+        }
+    }
+}
