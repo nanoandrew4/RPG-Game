576b8a6dde6c6c764323c7aeab452754ffbee100
AianC <aechen0@gmail.com>
2016-06-04 16:59:06 -0600

    MVC'd everything.

---
+++ InMap.java
@@ -0,0 +1,12 @@
+/*
+    Main class for InMap.
+ */
+
+package inmap;
+
+public class InMap {
+    public static void main(String[] args) {
+        InMapController control = new InMapController();
+        control.run();
+    }
+}
